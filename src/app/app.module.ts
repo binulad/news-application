205f59a67913e098e8cedba5a5942063c500ae1d
binallad <binallad19@gmail.com>
2023-10-05 16:20:56 +0530

    feat: Enable Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// import { AuthModule } from '@auth0/auth0-angular';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,13 +21,13 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     CoreModule,
     ModalModule.forRoot(),
-    // AuthModule.forRoot({
-    //   domain: 'dev-ynctyrjr13uwv5zv.us.auth0.com',
-    //   clientId: 'EvwqlKxj4RVKObQvxf8OvNRJpCUsKHyk',
-    //   authorizationParams: {
-    //     redirect_uri: window.location.origin,
-    //   },
-    // }),
+    AuthModule.forRoot({
+      domain: 'dev-ynctyrjr13uwv5zv.us.auth0.com',
+      clientId: 'EvwqlKxj4RVKObQvxf8OvNRJpCUsKHyk',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
